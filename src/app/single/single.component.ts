e5a822bbfe835301112df5bfcacbcfad22c8a6d0
osama-mahmoud-h <om1408@fayoum.edu.eg>
2022-12-30 13:57:30 +0200

    first commit

---
+++ src/app/single/single.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BackendCallsService } from '../backend-calls.service';
+import { VideoDto } from '../video-dto';
+
+@Component({
+  selector: 'app-single',
+  templateUrl: './single.component.html',
+  styleUrls: ['./single.component.css']
+})
+export class SingleComponent implements OnInit {
+  video: VideoDto | undefined;
+  constructor(private activated: ActivatedRoute, private videoService: BackendCallsService) {
+    const id = this.activated.snapshot.paramMap.get("videoId");
+    this.videoService.getVideo(`/api/videos/${id}`).subscribe(
+      res => {
+        this.video = res.data;
+        console.log(this.video);
+        console.log(res.data);
+      },
+      err => console.log(err)
+    );
+  }
+  ngOnInit(): void {
+  }
+
+}
